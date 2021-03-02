597c73f2a83abd4c457a79583c91319d302e1909
yoimada <rudenenet@yahoo.co.jp>
2021-03-03 06:07:31 +0900

    Update curls.sh

--- curls.sh
+++ curls.sh
@@ -5,25 +5,27 @@ rec()
 {
   NOW=$1
   OLD=`echo ./end/$NOW`
-  if [ -e $OLD ]; then
-          mv $NOW _$NOW
-          rec _$NOW
+  if [ -e $OLD ]; then 
+	  mv $NOW _$NOW
+	  rec _$NOW
   fi
   return;
 }
 
 mkdir -p ./end;
-for i in $(cat ../url.txt) ;
-do
-        curl -sS --ciphers 'DEFAULT:!DH' -O $(echo $i | tr '\r' ' ') ;
-        if [ "$?" = "0" ]; then
-                NOW=`/bin/ls -rt1 | grep -v curls.sh | tail -1`
-                rec $NOW
-                FILENAME=`/bin/ls -rt1 | grep -v curls.sh | tail -1`
-                mv $FILENAME ./end/
-        else
-                exit 1;
-        fi
+for i in $(cat ../url.txt) ; 
+do 
+	curl -sS --ciphers 'DEFAULT:!DH' -O $(echo $i | tr '\r' ' ') ; 
+	if [ "$?" = "0" ]; then
+		NOW=`/bin/ls -rt1 | grep -v curls.sh | tail -1`
+		rec $NOW
+		FILENAME=`/bin/ls -rt1 | grep -v curls.sh | tail -1`
+		mv $FILENAME ./end/
+		echo $FILENAME
+	else 
+		echo "fail to curl: $i"
+	fi
 done
 
 exit 0;
+
