0088f0925abe0d67ccfb6217c43bb353e2ab1c33
yoimada <rudenenet@yahoo.co.jp>
2026-07-08 01:20:14 +0900

    Update curls.sh

--- curls.sh
+++ curls.sh
@@ -13,17 +13,36 @@ rec()
 }
 
 mkdir -p ./end;
-for i in $(cat ../url.txt) ; 
-do 
-	curl -sS --ciphers 'DEFAULT:!DH' -O $(echo $i | tr '\r' ' ') ; 
+mkdir -p ./fail;
+
+# 総件数を取得
+TOTAL=$(wc -l < ../url.txt)
+COUNT=0
+
+for i in $(cat ../url.txt) ; do
+	COUNT=$((COUNT + 1))
+
+	sleep 2;
+	curl -A "Mozilla/5.0" -sS --ciphers 'DEFAULT:!DH' -O $(echo $i | tr '\r' ' ') ;
+
 	if [ "$?" = "0" ]; then
 		NOW=`/bin/ls -rt1 | grep -v curls.sh | tail -1`
 		rec $NOW
 		FILENAME=`/bin/ls -rt1 | grep -v curls.sh | tail -1`
-		mv $FILENAME ./end/
-		echo $FILENAME
-	else 
-		echo "fail to curl: $i"
+
+		# file 判定（text を含む＝HTML＝失敗）
+			if file "$FILENAME" | grep -qi "text"; then
+				echo "[${COUNT}/${TOTAL}] FAIL (HTML detected): $FILENAME"
+				mv "$FILENAME" ./fail/
+				continue
+			fi
+
+			# 正常ファイル
+			mv "$FILENAME" ./end/
+			echo "[${COUNT}/${TOTAL}] OK: $FILENAME"
+
+	else
+		echo "[${COUNT}/${TOTAL}] curl failed: $i"
 	fi
 done
 
